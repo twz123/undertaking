d3d2738d54be0f7863dbc1921a8f5340b2b1f99e
Christoph Berg <christoph.berg@zalando.de>
2016-09-08 10:16:41 +0200

    Let OAuth2Settings inherit from the splitted settings classes

--- src/main/java/org/zalando/undertaking/oauth2/OAuth2Settings.java
+++ src/main/java/org/zalando/undertaking/oauth2/OAuth2Settings.java
@@ -1,9 +1,5 @@
 package org.zalando.undertaking.oauth2;
 
-import java.net.URI;
-
-import java.util.Set;
-
 /**
  * Provides configuration settings for the OAuth2 functionality.
  *
@@ -12,35 +8,4 @@ import java.util.Set;
  *              scheduled for removal in Undertaking 0.1.0.
  */
 @Deprecated
-public interface OAuth2Settings {
-
-    /**
-     * URL to request a new access token creation.
-     */
-    URI getAccessTokenEndpoint();
-
-    /**
-     * URL to validate and request information about access tokens.
-     */
-    URI getTokenInfoEndpoint();
-
-    /**
-     * OAuth scopes to request the token with.
-     */
-    Set<String> getAccessTokenScopes();
-
-    /**
-     * Percentage value of the validity period when the token should be refreshed.
-     */
-    int getRefreshTokenPercentage();
-
-    /**
-     * Name of the HTTP header being used to overwrite the business partner identifier.
-     */
-    String getBusinessPartnerIdOverrideHeader();
-
-    /**
-     * Name of scope being required to overwrite the business partner identifier.
-     */
-    String getBusinessPartnerIdOverrideScope();
-}
+public interface OAuth2Settings extends AccessTokenSettings, AuthenticationInfoSettings { }
