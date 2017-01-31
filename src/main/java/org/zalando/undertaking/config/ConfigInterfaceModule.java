a019bcd93f9acc6d6220631f895df70914251f63
Tobias Bahls <tobias.bahls@zalando.de>
2017-01-31 14:21:33 +0100

    Review Comments
    * Added some javadoc detailing on how to use this class
    * Use requireNonNull and made static factories consistent

--- src/main/java/org/zalando/undertaking/config/ConfigInterfaceModule.java
+++ src/main/java/org/zalando/undertaking/config/ConfigInterfaceModule.java
@@ -3,6 +3,7 @@ package org.zalando.undertaking.config;
 import java.util.Arrays;
 import java.util.Collections;
 
+import com.google.inject.Binder;
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
 
@@ -11,13 +12,46 @@ import com.google.inject.Module;
 import com.google.inject.Singleton;
 import com.google.inject.binder.ScopedBindingBuilder;
 
+import static java.util.Objects.requireNonNull;
+
 /**
- * Creates bindings for configuration interfaces to be dynamically implemented by the {@code owner} library.
+ * <p>
+ *   Creates bindings for configuration interfaces to be dynamically implemented by the {@code owner} library.
+ * </p>
+ *
+ * <p>
+ *  The configuration is set up in a way that it automatically resolves system environment properties in addition to
+ *  resolving any <code>{@literal @}LoadStrategy</code> annotations set on the <code>Configuration</code> interfaces.
+ * </p>
+ *
+ * Usage:
+ * <ul>
+ *  <li>Define an Interface for holding the configuration, see <a href="http://owner.aeonbits.org/docs/usage/">Owner Usage Documentation</a></li>
+ *  <li>Use this class in a Guice module to bind the interface</li>
+ *  <li>Use <code>@Inject</code> to get injected instances of the configuration.</li>
+ * </ul>
+ *
+ * Example, assuming the <code>ServerConfig</code> interface from the Usage Documentation:
+ * <pre>
+ *     public class TestModule extends Module {
+ *        {@literal @}Override
+ *         public void configure(Binder binder) {
+ *             install(ConfigInterfaceModule.with(ServerConfig.class));
+ *         }
+ *     }
+ *
+ *     public class MyTestConsumer {
+ *        {@literal @}Inject
+ *         private ServerConfig config;
+ *
+ *         // rest omitted for brevity
+ *     }
+ * </pre>
  */
 public final class ConfigInterfaceModule extends AbstractModule {
     private final Iterable<? extends Class<? extends Config>> configInterfaces;
 
-    public static <T extends Config> Module with(final Class<T> configInterface) {
+    public static Module with(final Class<? extends Config> configInterface) {
         return with(Collections.singletonList(configInterface));
     }
 
@@ -30,7 +64,7 @@ public final class ConfigInterfaceModule extends AbstractModule {
     }
 
     private ConfigInterfaceModule(final Iterable<? extends Class<? extends Config>> configInterfaces) {
-        this.configInterfaces = configInterfaces;
+        this.configInterfaces = requireNonNull(configInterfaces);
     }
 
     @Override
